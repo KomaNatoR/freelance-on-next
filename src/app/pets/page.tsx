c032a58ff85d50b43e6c9dd082f55476bbb40ca9
Serhii Markov <goo.komalel@gmail.com>
2023-12-06 20:05:19 +0200

    prepared to work!
    -created start window.
    -added sass.
    -some fixes.

---
+++ src/app/pets/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Pets = () => {
+    return (
+        <>
+            <header></header>
+            
+            <main>
+                {"PETS!"}
+            </main>
+
+            <footer></footer>
+        </>
+    )
+};
+
+export default Pets;
---
+++ src/ui/main-components/main-navigate/main-navigate.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+import variables from './main-nav.module.scss';
+
+
+const MainNavigate = () => {
+    return (
+        <div  className={variables.cont}>
+            <h1 className={variables.h1}>
+                {"Hello it's my HTML/SCSS/NEXT practic!"}
+            </h1>
+
+            <nav className={variables.nav}>
+                <Link href="/pets" className={variables.link}>pets</Link>
+            </nav>
+        </div>
+    )
+};
+
+export default MainNavigate;
