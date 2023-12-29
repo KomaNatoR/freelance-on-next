41624110112f4877ea3c10c533e3f8f8056c9a48
Serhii Markov <goo.komalel@gmail.com>
2023-12-29 15:39:48 +0200

    added svgs!

--- src/ui/wild_kingdom/body/header/Header.tsx
+++ src/ui/wild_kingdom/body/header/Header.tsx
@@ -1,26 +1,32 @@
-"use client"
+"use client";
+import Image from 'next/image';
+import useMedia from 'use-media';
 
 import NavHeader from './compon_header/NavHeader';
 import Logo from './compon_header/Logo';
 import Auth from './compon_header/Auth';
 
 import styles from './header.module.scss';
+import burger from '@/img/wild/icons/menu_burger.svg';
 
 
 const Header = () => {
+  const isMobile = useMedia("(max-width: 767px)");
+
 
   return (
     <header className={styles.header}>
+      <div className={styles.container}>
 
-      <div  className={styles.container}>
-        <NavHeader />
-        
+        {!isMobile && <NavHeader />}
         <Logo />
+        {!isMobile && <Auth />}
 
-        <Auth />
+        {isMobile && <Image src={burger} alt='burger'/>}
+
       </div>
     </header>
   )
-}
+};
 
 export default Header;
--- src/ui/wild_kingdom/body/header/compon_header/Logo.tsx
+++ src/ui/wild_kingdom/body/header/compon_header/Logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import MediaQuery from 'react-responsive'
+// import MediaQuery from 'react-responsive';
 
 import logo from '../../../../../img/icons/logo.svg';
 
@@ -7,22 +7,22 @@ import logo from '../../../../../img/icons/logo.svg';
 const Logo = () => {
     return (
         <>
-            <MediaQuery maxWidth={767}>
+            {/* <MediaQuery maxWidth={767}>
                 <Image
                     src={logo}
                     width={108}
                     height={38}
                     alt="Logo"
                 />
-            </MediaQuery>
-            <MediaQuery minWidth={768}>
+            </MediaQuery> */}
+            {/* <MediaQuery minWidth={768}> */}
                 <Image
                     src={logo}
                     width={216}
                     height={77}
                     alt="Logo"
                 />
-            </MediaQuery>
+            {/* </MediaQuery> */}
         </>
     )
 };
