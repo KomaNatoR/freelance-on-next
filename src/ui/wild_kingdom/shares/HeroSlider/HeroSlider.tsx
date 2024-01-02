09a562b02ceddfa9b54adf15c7646078435600e7
Serhii Markov <goo.komalel@gmail.com>
2024-01-02 21:00:08 +0200

    created number cycling!

--- src/ui/wild_kingdom/shares/HeroSlider/HeroSlider.tsx
+++ src/ui/wild_kingdom/shares/HeroSlider/HeroSlider.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useState } from "react";
 import Image from "next/image";
 import arrow from './img/arrow.svg'
 
@@ -6,31 +7,51 @@ import styles from './slider.module.scss'
 import { montserrat } from "@/ui/fonts";
 
 
+type Arr = string[];
+let cicleringArr: Arr = ['1', '2', '3', '4'];
+
+
 const HeroSlider = () => {
+    const [visualArr, setVisualArr] = useState<Arr>(cicleringArr);
+
+    const handleButtBack = (arr: Arr): void => {
+        
+        const newArr = [...arr];
+        newArr.push(newArr.shift()!);
+        setVisualArr(newArr);
+        // console.log('|  <--  |', newArr);
+    }
+    const handleButtStright = (arr: Arr): void => {
+        
+        const newArr = [...arr];
+        newArr.unshift(newArr.pop()!);
+        setVisualArr(newArr);
+        // console.log('|  -->  |', newArr);
+    }
 
 
     return (
         <div className={`${styles.hero_slider} ${montserrat.className}`}>
 
             <div className={styles.big_circle}>
-                <p>{'1'}</p>
+                <p>{visualArr[0]}</p>
             </div>
           
             <div className={styles.small_circle}>
-                <p>{'2'}</p>
+                <p>{visualArr[1]}</p>
             </div>
             <div className={styles.small_circle}>
-                <p>{'3'}</p>
+                <p>{visualArr[2]}</p>
             </div>
             <div className={styles.small_circle}>
-                <p>{'4'}</p>
+                <p>{visualArr[3]}</p>
             </div>
 
             <div className={styles.butt_box}>
-                <button type='button' className={styles.butt_first}>
+                <button type='button' className={styles.butt_first} onClick={()=>handleButtBack(visualArr)}>
                     <Image src={arrow} alt='Arrow Left' width={34} height={16} />
                 </button>
-                <button type='button' className={styles.butt_second}>
+                <button type='button' className={styles.butt_second} onClick={()=>handleButtStright(visualArr)}>
                     <Image src={arrow} alt='Arrow Right' width={54} height={26} />
                 </button>
             </div>
