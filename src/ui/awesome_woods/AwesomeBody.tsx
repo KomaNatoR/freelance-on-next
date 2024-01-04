3922a61bc3b2fb5d69a297e3d27a75ad5e232087
Serhii Markov <goo.komalel@gmail.com>
2024-01-04 16:46:55 +0200

    done animation!

--- src/ui/awesome_woods/AwesomeBody.tsx
+++ src/ui/awesome_woods/AwesomeBody.tsx
@@ -1,11 +1,20 @@
+'use client'
+import { useEffect } from "react";
 
 import styles from "./awesome.module.scss";
 
 
 const AwesomeBody = () => {
+    useEffect(() => {
+        const container = document.getElementById('animatedContainer');
+        if (container) {
+            container.classList.add(styles.animateIn);
+        }
+    }, []);
+
     return (
-        <div className={styles.body}>
-            <div className={`${styles.wrapper} ${styles}`}>
+        <div id="animatedContainer" className={styles.body}>
+            <div className={`${styles.wrapper}`}>
 
                 <main className={styles.page}>
                     <div className={`${styles.main_screen} ${styles.page_main_screen}`}>
@@ -16,13 +25,13 @@ const AwesomeBody = () => {
                         <div className={`${styles.trees} ${styles.two}`}><span></span></div>
                         <div className={`${styles.trees} ${styles.one}`}><span></span></div>
 
-                        <div className="birds 1"><span></span></div>
-                        <div className="birds 2"><span></span></div>
+                        <div className={`${styles.birds} ${styles.bird_one}`}><span></span></div>
+                        <div className={`${styles.birds} ${styles.bird_two}`}><span></span></div>
 
-                        <div className="mountain"><span></span></div>
+                        <div className={`${styles.mountain}`}><span></span></div>
 
-                        <section className="page_content">
-                            <div className="content_container">
+                        <section className={`${styles.page_content} ${styles.content}`}>
+                            <div className={styles.content_container}>
                                 <h1>Code only</h1>
                                 <div className="content_body">
                                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates corrupti eveniet minus nobis qui molestiae a amet. Dignissimos quis sed perferendis corrupti reprehenderit quidem accusantium minus labore dolore? A, tempore?</p>
@@ -46,7 +55,6 @@ const AwesomeBody = () => {
                     </div>
 
                 </main>
-
             </div>
         </div>
     )
